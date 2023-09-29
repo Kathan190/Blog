3ed0a420b31f6dd4ff4790b26d36f31fbcaac690
iamkathan <shethkathan4@gmail.com>
2023-09-29 08:48:37 +0100

    header has been added

--- Home/views.py
+++ Home/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("this is home page")
+    return render(request, 'index.html')
+    #return HttpResponse("this is home page")
