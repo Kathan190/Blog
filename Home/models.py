769a7ae60dbb454db81718fb9918becb0a2b8a73
kathan190 <shethkathan4@gmail.com>
2024-07-25 00:44:28 +0100

    database has been created for blog post

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog1(models.Model):
+    sno = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.title
+    
+class Blog2(models.Model):
+    sno = models.AutoField(primary_key=True)
+    heading = models.CharField(max_length=100)
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.title
--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
-
+from Home.models import Blog1
+from Home.models import Blog2
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    allBlog1 = Blog1.objects.all()
+    allBlog2 = Blog2.objects.all()
+    context = {'allBlog1':allBlog1, 'allBlog2':allBlog2}
+    return render(request, 'index.html', context)
     #return HttpResponse('This is blog page')
