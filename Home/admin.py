67e58555859b22c2353e9dfb7786a843d960e08c
kathan190 <shethkathan4@gmail.com>
2024-07-25 03:16:18 +0100

    half contact section has been done

--- Home/admin.py
+++ Home/admin.py
@@ -1,7 +1,2 @@
 from django.contrib import admin
 from Home.models import Blog1
-from Home.models import Blog2
-# Register your models here.
-
-admin.site.register(Blog1)
-admin.site.register(Blog2)
--- Home/views.py
+++ Home/views.py
@@ -3,8 +3,4 @@ from Home.models import Blog1
 from Home.models import Blog2
 # Create your views here.
 def index(request):
-    allBlog1 = Blog1.objects.all()
-    allBlog2 = Blog2.objects.all()
-    context = {'allBlog1':allBlog1, 'allBlog2':allBlog2}
-    return render(request, 'index.html', context)
-    #return HttpResponse('This is blog page')
+    allBlog1 = Blog1.objects
