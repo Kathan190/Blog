75fc2c0ec0e701b9e5e1acea6856d23ad83ff8b6
kathan190 <shethkathan4@gmail.com>
2024-07-25 23:52:13 +0100

    contact section has been done with the maps

--- Home/admin.py
+++ Home/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
 from Home.models import Blog1
+from Home.models import Blog2
+# Register your models here.
+
+admin.site.register(Blog1)
+admin.site.register(Blog2)
